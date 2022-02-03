dd0e5ce5b1e622eaa2529c56a3f0ea57fe4db1e5
Thiago Fontoura <thiagoatf@hotmail.com>
2022-02-03 16:38:18 -0300

    Add project files.

---
+++ Contract/Services/ContractService.cs
@@ -0,0 +1,27 @@
+﻿using Contract.Entities;
+using System;
+
+namespace Contract.Services
+{
+    class ContractService
+    {
+        private IOnlinePayment _IOnlinePayment;
+
+        public ContractService(IOnlinePayment iOnlinePayment)
+        {
+            _IOnlinePayment = iOnlinePayment;
+        }
+
+        public void ProcessContract(Contracts contract, int month)
+        {
+            double basicQuota = contract.TotalValue / month;
+            for(int i = 1;i <= month; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _IOnlinePayment.Interest(basicQuota, i);
+                double fullQuota = updatedQuota + _IOnlinePayment.PaymentFee(updatedQuota);
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
---
+++ Contract/Services/IOnlinePayment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contract.Services
+{
+    interface IOnlinePayment 
+    {
+        double PaymentFee(Double amount);
+        double Interest(double amount, int month);
+    }
+}
